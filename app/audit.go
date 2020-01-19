ff0751c8090fcebb42b8c8f1a4546c88a47c9c60
Jesús Espino <jespinog@gmail.com>
2020-01-19 14:21:07 +0100

    Removing unnused method

--- app/audit.go
+++ app/audit.go
@@ -7,10 +7,6 @@ import (
 	"github.com/mattermost/mattermost-server/v5/model"
 )
 
-func (a *App) GetAudits(userId string, limit int) (model.Audits, *model.AppError) {
-	return a.Srv.Store.Audit().Get(userId, 0, limit)
-}
-
 func (a *App) GetAuditsPage(userId string, page int, perPage int) (model.Audits, *model.AppError) {
 	return a.Srv.Store.Audit().Get(userId, page*perPage, perPage)
 }
